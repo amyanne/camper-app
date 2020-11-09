bff94b4cb8b459993b076921c4a38565f2c9c605
amyanne <amywestcott01@gmail.com>
2020-11-09 14:09:21 -0800

    another commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Nav from './components/Nav';
+import Map from './containers/MapContainer';
 import './App.css';
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
       <div className="container" align="center">
         <Switch>
           <Route exact path="/" component={ Home } />
+          <Route exact path="/map" component={ Map } />
          
         </Switch>
       </div>
